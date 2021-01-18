7474f9392633161301f0c6a54b13372a5d186dad
soleperson <h2magic@126.com>
2021-01-18 14:16:44 +0800

    build database structure

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+class Article(models.Model):
+    title = models.CharField(max_length=70)
+    excerpt = models.CharField(max_length=200, blank=True)
+    created_time = models.DateTimeField()
+    content = models.TextField()
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+
