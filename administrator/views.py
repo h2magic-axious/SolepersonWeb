91aea59e790c73b6728f2fe5f87710887488348f
soleperson <h2magic@126.com>
2021-01-19 16:53:00 +0800

    Finish the article management page

--- administrator/views.py
+++ administrator/views.py
@@ -3,7 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def admin_index(request):
-    return render(request, 'blogadmin/admin_blog_app.html', context={
-        'admin_position': 'Test Admin Page',
-        'breadcrumb_list': ['2021', '01', '19']
-    })
+    return render(request, 'blogadmin/admin_index.html')
