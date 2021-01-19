c3580e89f959947d996010071cec0b057ec4bc05
soleperson <h2magic@126.com>
2021-01-19 16:17:24 +0800

    sort out the aside menus

--- administrator/views.py
+++ administrator/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def admin_index(request):
-    return render(request, 'blogadmin/admin_root.html', context={
+    return render(request, 'blogadmin/admin_blog_app.html', context={
         'admin_position': 'Test Admin Page',
         'breadcrumb_list': ['2021', '01', '19']
     })
