dd53aad87578ad6e6c06134e01a49e712c349e23
soleperson <h2magic@126.com>
2021-01-19 11:48:44 +0800

    using framework: AdminLTE

--- administrator/templatetags/administrator_tags.py
+++ administrator/templatetags/administrator_tags.py
@@ -8,3 +8,11 @@ register = template.Library()
 @register.simple_tag
 def administrator_category_list():
     return Category.objects.all()
+
+
+@register.simple_tag
+def breadcrumb_active(last):
+    if last:
+        return 'active'
+    else:
+        return ''
--- administrator/views.py
+++ administrator/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def admin_index(request):
-    return render(request, 'blogadmin/admin_root.html')
+    return render(request, 'blogadmin/admin_root.html', context={
+        'admin_position': 'Test Admin Page',
+        'breadcrumb_list': ['2021', '01', '19']
+    })
