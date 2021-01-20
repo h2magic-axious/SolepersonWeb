854bc48ce988f578a64cf7210b5f8332d78ea7ad
soleperson <h2magic@126.com>
2021-01-20 10:49:25 +0800

    init vue project for web management

--- administrator/urls.py
+++ administrator/urls.py
@@ -4,10 +4,5 @@ from administrator import views
 
 app_name = 'admin'
 urlpatterns = [
-    path('', views.admin_index, name="admin_index"),
-    path('article_edit/', views.admin_article_edit, name='admin_article_edit'),
-    path('categories_list/', views.admin_categories, name='admin_categories_list'),
-    path('tags_list/', views.admin_tags, name='admin_tags_list'),
-    # Form urls
-    path('form/category/edit/<int:pk>/', views.admin_category_post, name='admin_category_edit'),
+    path('category-list/', views.category_list),
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -1,30 +1,18 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 from blogapp.models import Category, Tag
 
 
-# Create your views here.
-def admin_index(request):
-    return render(request, 'blogadmin/admin_index.html')
-
-
-def admin_article_edit(request):
-    return render(request, 'blogadmin/admin_blog_app_edit.html')
-
-
-def admin_categories(request):
-    return render(request, 'blogadmin/admin_blog_category.html')
-
-
-def admin_tags(request):
-    return render(request, 'blogadmin/admin_blog_tag.html')
-
-
-# Form functions
-def admin_category_post(request, pk):
-    category = get_object_or_404(Category, pk=pk) if pk else Category()
-    if request.method == 'POST':
-        category.name = request.POST['category_name']
-        category.save()
-
-    return redirect('admin:admin_categories_list')
+def category_list(request):
+    return JsonResponse({
+        'count': len(Category.objects.all()),
+        'results': [
+            {
+                'id': category.pk,
+                'name': category.name,
+                'articleNumber': len(category.article_set.all())
+            }
+            for category in Category.objects.all()
+        ]
+    })
