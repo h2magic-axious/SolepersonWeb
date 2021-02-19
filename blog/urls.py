2a6de6f7419b419a7eb2fb5bf088555f528b6a8a
soleperson <h2magic@126.com>
2021-02-19 11:44:58 +0800

    support blog sitemap

--- blog/urls.py
+++ blog/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin, sitemaps
-from django.contrib.sitemaps.views import sitemap
+from django.contrib import admin  # , sitemaps
+# from django.contrib.sitemaps.views import sitemap
 
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    # path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('', include('blogapp.urls')),
     path('', include('comment.urls')),
 ]
--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import GenericSitemap
+
 from blogapp import views
+from blogapp.models import Article
+
+info_dict = {
+    'queryset': Article.objects.all(),
+    'date_field': 'created_time'
+}
 
 app_name = 'blog_app'
 urlpatterns = [
@@ -10,5 +19,10 @@ urlpatterns = [
     path('category/<int:pk>/', views.CategoryView.as_view(), name='category'),
     path('tags/<int:pk>/', views.TagView.as_view(), name='tag'),
     path('search/', views.search, name='search'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+
+    path('sitemap.xml', sitemap, {
+        'sitemaps': {
+            'blog': GenericSitemap(info_dict, priority=0.6)
+        }}, name='django.contrib.sitemaps.views.sitemap')
 ]
